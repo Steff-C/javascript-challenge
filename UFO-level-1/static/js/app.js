f7c7317c93a216d92e2fb85f4483fa452fb78e3d
Steff-C <stephanie.core@mortenson.com>
2021-04-21 21:38:07 -0500

    statring code to populate table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,23 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+var button = d3.select("filter-btn")
+var inputdate = d3.select("#datetime");
+var inputcity = d3d3.select("#city");
+var resetbtn = d3.select("#reset-btn");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+var populate = (inputData) => {
+        inputData.foreach(et_sightings => {
+            var row = tbody.append("tr");
+            columns.forEach(column => row.append("td").text(et_sightings[column])
+            )
+        });
+}
+
+populate(data);
+
+
+
