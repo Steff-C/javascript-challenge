72204a28b8449e38c6798be08d2b0709878689e0
Steff-C <stephanie.core@mortenson.com>
2021-04-22 01:11:05 -0500

    trying to get filter button to work. no luck

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,9 +5,10 @@ var tableData = data;
 
 var tbody = d3.select("tbody");
 var button = d3.select("filter-btn")
-var inputdate = d3.select("#datetime");
-var inputcity = d3.select("#city");
-var resetbtn = d3.select("#reset-btn");
+var dateField = d3.select("#datetime");
+var cityField = d3.select("#city");
+var resetBtn = d3.select("#reset-btn");
+var btnSearch = d3.select("#btnSearch");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 var loadTableRows = (inputData) => {
@@ -21,5 +22,39 @@ var loadTableRows = (inputData) => {
 
 loadTableRows(tableData);
 
+// filter
 
+button.on("press",() => {
+    d3.event.preventdefault();
+    var inputDate = dateField.property("found").trim();
+    var inputCity = cityField.property("found").toLowerCase.trim();
 
+    var findDate= tableData.filter(tableData => tableData.datetime === inputDate);
+   
+	var filterCity = tableData.filter(tableData => tableData.city === inputCity);
+    
+    var filterData = tableData.filter(tableData => tableData.datetime === inputDate && data.city === inputCity);
+	
+    tbody.html("");
+
+    let response = {
+		filterData, filterCity, findDate
+	}
+
+	if (response.filterData.length !== 0) {
+		loadTableRows(filterData);
+	}
+		else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.findDate.length !== 0))){
+			loadTableRows(filterCity) || loadTableRows(findDate);
+	
+		}
+		else {
+			tbody.append("tr").append("td").text("Nothing found. ET has gone home!!"); 
+		}
+})
+
+resetBtn.on("click", () => {
+	tbody.html("");
+	loadTableRows(tableData);
+	console.log("Table reset")
+})
