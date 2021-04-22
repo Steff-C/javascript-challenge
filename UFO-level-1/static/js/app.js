406512215412739e2c9c1a924f8b8002c59a1ece
Steff-C <stephanie.core@mortenson.com>
2021-04-21 22:44:06 -0500

    finally got able to load

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -6,19 +6,20 @@ var tableData = data;
 var tbody = d3.select("tbody");
 var button = d3.select("filter-btn")
 var inputdate = d3.select("#datetime");
-var inputcity = d3d3.select("#city");
+var inputcity = d3.select("#city");
 var resetbtn = d3.select("#reset-btn");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
-var populate = (inputData) => {
-        inputData.foreach(et_sightings => {
+var loadTableRows = (inputData) => {
+    tbody.html("");
+        inputData.forEach(et_sightings => {
             var row = tbody.append("tr");
             columns.forEach(column => row.append("td").text(et_sightings[column])
             )
         });
 }
 
-populate(data);
+loadTableRows(tableData);
 
 
 
