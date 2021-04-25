fe40d738b2534f025b8eaafc96be25a8df4dd0f9
Steff-C <stephanie.core@mortenson.com>
2021-04-24 22:07:58 -0500

    starting new code with const

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,6 +1,20 @@
 console.log("ET")
 
-var tableData = data;
-
 // YOUR CODE HERE!
 
+const tableData = data
+const tbody = d3.select("tbody")
+let filters = {}
+
+
+function loadTableRows(data) {
+    tbody.html("")  
+
+    data.forEach((row) => {
+        const currentRow = tbody.append("tr")  
+        Object.values(row).forEach((value) => {
+            let cell = currentRow.append("td")
+            cell.text(value)
+        })
+    })
+}
