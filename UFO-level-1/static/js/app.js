22e6975ba518900b7640d0c0aaa86a8f4ac0c6dc
Steff-C <stephanie.core@mortenson.com>
2021-04-24 22:25:46 -0500

    add filter data

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -35,4 +35,29 @@ const handleClick = () => {
     console.log(`Country: ${countryInput}`)
     console.log(`Shape: ${shapeInput}`)
 
+    if (dateInput) {
+        filteredData = filteredData.filter((row) => row.datetime === dateInput)
+    }
+    if (cityInput) {
+        filteredData = filteredData.filter((row) => row.city === cityInput)
+    }
+    if (stateInput) {
+        filteredData = filteredData.filter((row) => row.state.toUpperCase() === stateInput)
+    }
+    if (countryInput) {
+        filteredData = filteredData.filter((row) => row.country.toUpperCase() === countryInput)
+    }
+    if (shapeInput) {
+        filteredData = filteredData.filter((row) => row.shape === shapeInput)
+    }
+
+    tbody.html('')  // Clear existing data
+
+    filteredData.forEach((row) => {
+        let currentRow = tbody.append('tr')  // append row
+        Object.values(row).forEach((value) => {
+            let cell = currentRow.append('td')
+            cell.text(value)
+        })
+    })
 }
