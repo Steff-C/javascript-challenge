15554a01478bded0adb688d198134f05bff617a8
Steff-C <stephanie.core@mortenson.com>
2021-04-25 01:25:43 -0500

    add reset to clear table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,11 +1,11 @@
-console.log("ET")
-
-// YOUR CODE HERE!
+console.log("E.T.");
 
 const tableData = data
 const tbody = d3.select("tbody")
+
 let filters = {}
 
+const resetBtn = d3.select("#clear");
 
 function loadTableRows(data) {
     tbody.html("")  
@@ -24,40 +24,31 @@ const handleClick = () => {
     let filteredTable = tableData;
 
     
-    let dateInput = d3.select("#datetime").property("value")
-    let cityInput = d3.select("#city").property("value")
-    let stateInput = d3.select("#state").property("value")
-    let countryInput = d3.select("#country").property("value")
-    let shapeInput = d3.select("#shape").property("value")
-    console.log(`Date: ${dateInput}`)
-    console.log(`City: ${cityInput}`)
-    console.log(`State: ${stateInput}`)
-    console.log(`Country: ${countryInput}`)
-    console.log(`Shape: ${shapeInput}`)
+    let dateInput = d3.select("#datetime").property("value");
+   
+    console.log(dateInput);
+    
 
     if (dateInput) {
-        filteredData = filteredData.filter((row) => row.datetime === dateInput)
-    }
-    if (cityInput) {
-        filteredData = filteredData.filter((row) => row.city === cityInput)
-    }
-    if (stateInput) {
-        filteredData = filteredData.filter((row) => row.state.toUpperCase() === stateInput)
-    }
-    if (countryInput) {
-        filteredData = filteredData.filter((row) => row.country.toUpperCase() === countryInput)
-    }
-    if (shapeInput) {
-        filteredData = filteredData.filter((row) => row.shape === shapeInput)
-    }
+        filteredTable = filteredTable.filter((row) => row.datetime === dateInput)
+    } 
 
-    tbody.html('')  // Clear existing data
+    tbody.html("")  
 
-    filteredData.forEach((row) => {
-        let currentRow = tbody.append('tr')  // append row
+    filteredTable.forEach((row) => {
+        let currentRow = tbody.append("tr")  
         Object.values(row).forEach((value) => {
-            let cell = currentRow.append('td')
+            let cell = currentRow.append("td")
             cell.text(value)
         })
     })
 }
+
+
+d3.selectAll("#submit").on("click", handleClick);
+loadTableRows(data);
+
+resetBtn.on("click", () => {
+    tbody.html("");
+    loadTableRows(data);
+    })
